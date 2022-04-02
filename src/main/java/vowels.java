457fd7daf46f6644ca79d08bab7e4de02e9bba42
Israel Garate <israelgarate@gmail.com>
2022-04-02 11:16:59 -0300

    ejercicio finalizado

--- src/main/java/vowels.java
+++ src/main/java/vowels.java
@@ -2,17 +2,14 @@ public class vowels {
 
     public static int countVowels(String str) {
        char[] v = str.toLowerCase().toCharArray();
-        int vowelsCount = 5;
+       char[] vowels = {'a','e','i','o','u'};
+        int vowelsCount = 0;
         for (int i = 0; i < str.length(); i++) {
 
-            if(v[i] == "a"){
-                vowelsCount--;
+            if(v[i] == 'a' || v[i] == 'e' || v[i] == 'i' || v[i] == 'o' || v[i] == 'u' ){
+                vowelsCount++;
             }
-
         }
-
-
-
         return vowelsCount;
     }
 
