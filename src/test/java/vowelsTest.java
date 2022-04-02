87d7462a7fc83a66d92e9528e5a8ef4309440463
Israel Garate <israelgarate@gmail.com>
2022-04-02 11:16:59 -0300

    ejercicio finalizado

--- src/test/java/vowelsTest.java
+++ src/test/java/vowelsTest.java
@@ -3,8 +3,16 @@ import static org.junit.Assert.assertEquals;
 
 public class vowelsTest {
     @Test
-    public void testCase1() {
+    public void countOnlyOneVowel() {
         assertEquals("Nope!", 5, vowels.countVowels("abracadabra"));
+    }
+    @Test
+    public void countAllVowels() {
+        assertEquals("Nope!", 5, vowels.countVowels("murcielago"));
+    }
+    @Test
+    public void dontCountVowels() {
+        assertEquals("Nope!", 0, vowels.countVowels("try"));
     }
 
 }
